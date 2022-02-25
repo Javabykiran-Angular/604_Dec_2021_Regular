06da213c8d1fc3f0ded41f029968fcf56860373b
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-02-25 10:15:01 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,20 @@
+// System.out.println("sfs")
+/** */
+// console.log('Hello World');
+var a1;
+var a2, a3;
+// Data Types
+//number(int,long,double,short,float), string('',""), boolean(true/false),
+//any (number,string,boolean),void(neutral ),
+// misec. null & undefined, 
+var b1 = 10; //Forward declration & defination
+// console.log("Value of b1 is "+b1)
+var b2;
+// console.log('value of B2 is '+b2)
+var b3; //Default data type is any
+b3 = 2.5;
+// console.log(`Value of b3 is ${b3}`);
+b3 = 'Sumit';
+// console.log(`Value of b3 is ${b3}`);
+b3 = true;
+console.log("Value of b3 is ".concat(b3));
