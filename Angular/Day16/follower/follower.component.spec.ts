17b44e7a0cc6abf02ae50449e383ca33fb365067
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-04-06 09:32:10 +0530

    Day16

---
+++ Angular/Day16/follower/follower.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FollowerComponent } from './follower.component';
+
+describe('FollowerComponent', () => {
+  let component: FollowerComponent;
+  let fixture: ComponentFixture<FollowerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FollowerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FollowerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Angular/Day16/query-parameter/query-parameter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-query-parameter',
+  templateUrl: './query-parameter.component.html',
+  styleUrls: ['./query-parameter.component.css']
+})
+export class QueryParameterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
